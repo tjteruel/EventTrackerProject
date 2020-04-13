17708a5b3ea53753b18df4361e0e548698dbeefe
Tim Teruel <timteruel@Administrators-MacBook-Pro.local>
2020-04-13 08:38:18 -0600

    controller

--- Workout/src/main/java/com/skilldistillery/workouts/WorkoutApplication.java
+++ Workout/src/main/java/com/skilldistillery/workouts/WorkoutApplication.java
@@ -2,9 +2,17 @@ package com.skilldistillery.workouts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.skilldistillery.workouts.entities.Workout;
 
 @SpringBootApplication
-public class WorkoutApplication {
+public class WorkoutApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WorkoutApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkoutApplication.class, args);
